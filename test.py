69bf50562f5f1a73293161a3d67153cd0256b29b
Ray <yenray.wen@gmail.com>
2021-06-19 15:52:38 +0800

    test

--- test.py
+++ test.py
@@ -1,10 +1,16 @@
-if __name__ == "main":
-    print("test case 1:")
-    x = 1
-    assert x == 2, \
-        exit(1)
-    print("test case 2:")
-    x = 3
-    assert x == 3, \
-        "wrong"
-    exit(0)
+print("test case 1:")
+x = 1
+if x != 1:
+    print("fail")
+    exit(1)
+# assert x == 1, \
+#     exit(1)
+print("test case 2:")
+x = 3
+if x != 3:
+    print("fail")
+    exit(1)
+# assert x == 3, \
+#     "wrong"
+print("test passed")
+exit(0)
