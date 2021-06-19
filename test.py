e6bf2a8793058a7b219a723bf3e3248eec0fd230
Ray <yenray.wen@gmail.com>
2021-06-19 15:54:28 +0800

    test

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
-print("test case 1:")
+print("test case 1:", end=" ")
 x = 1
 if x != 1:
     print("fail")
     exit(1)
+print("... pass")
 # assert x == 1, \
 #     exit(1)
-print("test case 2:")
+print("test case 2:", end=" ")
 x = 3
 if x != 3:
     print("fail")
