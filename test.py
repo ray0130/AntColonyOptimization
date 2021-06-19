68af23054a2f1f4234eb93af1d02430a1f4b8232
Ray <yenray.wen@gmail.com>
2021-06-19 15:27:33 +0800

    add hooks

---
+++ test.py
@@ -0,0 +1,10 @@
+if __name__ == "main":
+    print("test case 1:")
+    x = 1
+    assert x == 2, \
+        "wrong"
+    print("test case 2:")
+    x = 3
+    assert x == 3, \
+        "wrong"
+    exit(0)
